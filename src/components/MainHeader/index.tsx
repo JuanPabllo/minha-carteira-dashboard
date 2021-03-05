5e33c9cadff8b4a9627712e528f6c171dec11b91
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-03-04 22:02:40 -0300

    add style on mainHeader and add random emoji

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-import { Container } from './styles';
+import { emojis } from '../../utils/emojis';
+
+import { Container, Profile, Welcome, UserName } from './styles';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Juan Pablo</UserName>
+      </Profile>
     </Container>
   );
 };
