f220b2d15ec800e7a9e72f12e137c7657c798efe
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-03-26 14:13:36 -0300

    fix: replace colors eventuais and recorrentes

--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -31,7 +31,7 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
   }
 
   .tag-filter-eventual::after {
@@ -39,6 +39,6 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
   }
 `;
