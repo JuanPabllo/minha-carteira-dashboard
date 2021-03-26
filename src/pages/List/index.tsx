754012e7e3ccac7da385623aee206d50824b86a9
juanpabllo <juanpablo192.dreaw@gmail.com>
2021-03-26 16:36:59 -0300

    Treating the month and year filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -32,12 +32,8 @@ interface IData {
 
 const List: React.FC<iRoutesParams> = ({ match }) => {
   const [data, setData] = useState<IData[]>([]);
-  const [monthSelected, setMonthSelected] = useState<string>(
-    String(new Date().getMonth() + 1)
-  );
-  const [yearSelected, setYearSelected] = useState<string>(
-    String(new Date().getFullYear())
-  );
+  const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+  const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
   const [frequencyselectedFilter, setFrequencyselectedFilter] = useState(['recorrente', 'eventual']);
 
   const movimentType = match.params.type;
@@ -102,13 +98,31 @@ const List: React.FC<iRoutesParams> = ({ match }) => {
     }
   };
 
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch(error) {
+      throw new Error('invalid month value. Is accept 0 - 24.');
+    }
+  }
+
+  const handleYearSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch(error) {
+      throw new Error('invalid year value. Is accept integer number.');
+    }
+  }
+
   useEffect(() => {
     const { data } = pageData;
 
     const filteredDate = data.filter((item) => {
       const date = new Date(item.date);
-      const month = String(date.getMonth() + 1);
-      const year = String(date.getFullYear());
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
 
       return month === monthSelected && year === yearSelected && frequencyselectedFilter.includes(item.frequency);
     });
@@ -131,12 +145,12 @@ const List: React.FC<iRoutesParams> = ({ match }) => {
       <ContentHeader title={pageData.title} lineColor={pageData.lineColor}>
         <SelectInput
           options={months}
-          onChange={(e) => setMonthSelected(e.target.value)}
+          onChange={(e) => handleMonthSelected(e.target.value)}
           defaultValue={monthSelected}
         />
         <SelectInput
           options={years}
-          onChange={(e) => setYearSelected(e.target.value)}
+          onChange={(e) => handleYearSelected(e.target.value)}
           defaultValue={yearSelected}
         />
       </ContentHeader>
