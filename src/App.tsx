f9ca292fe19a8ac49cc3575bda82f6cc39533762
Juan Pablo <59495901+JuanPabllo@users.noreply.github.com>
2021-04-10 13:38:19 -0300

    some adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import Routes from './routes';
 
 import { dark } from './styles/theme/dark';
-import { light } from './styles/theme/light';
+// import { light } from './styles/theme/light';
 
 const App: React.FC = () => {
   return (
