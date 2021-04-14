3714b90fb7d9f980133b75c30518c83e796df8ff
Juan Pablo <59495901+JuanPabllo@users.noreply.github.com>
2021-04-14 15:39:55 -0300

    create Component BarChartBox

---
+++ src/components/BarChartBox/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { ResponsiveContainer, BarChart, Bar, Cell, Tooltip } from 'recharts';
+
+import formatCurrency from '../../utils/formatCurrency';
+
+import {
+  Container,
+  SideLeft,
+  LegendContainer,
+  Legend,
+  SideRight,
+} from './styles';
+
+interface IBarChartBoxProps {
+  title: string;
+  data: {
+    name: string;
+    amount: number;
+    percent: number;
+    color: string;
+  }[];
+}
+
+const BarChartBox: React.FC<IBarChartBoxProps> = ({ title, data }) => {
+  return (
+    <Container>
+      <SideLeft>
+        <h2>{title}</h2>
+
+        <LegendContainer>
+          {data.map((indicator) => (
+            <Legend key={indicator.name} color={indicator.color}>
+              <div>{indicator.percent}%</div>
+              <span>{indicator.name}</span>
+            </Legend>
+          ))}
+        </LegendContainer>
+      </SideLeft>
+
+      <SideRight>
+        <ResponsiveContainer>
+          <BarChart data={data}>
+            <Bar dataKey="amount">
+              {data.map((indicator) => (
+                <Cell
+                  key={indicator.name}
+                  fill={indicator.color}
+                  cursor="pointer"
+                />
+              ))}
+            </Bar>
+            {/* <Tooltip
+              formatter={(value: number) => formatCurrency(Number(value))}
+            /> */}
+          </BarChart>
+        </ResponsiveContainer>
+      </SideRight>
+    </Container>
+  );
+};
+
+export default BarChartBox;
