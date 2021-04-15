3d7952ba447f50fc6a0d7ecfd8f9d60703b45db6
Juan Pablo <59495901+JuanPabllo@users.noreply.github.com>
2021-04-15 15:31:28 -0300

    Changing the theme for the Toggle button

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,14 +1,28 @@
-import React, { useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
+
+import { useTheme } from '../../hooks/useTheme';
 
 import Toggle from '../Toggle';
 import { emojis } from '../../utils/emojis';
+
 import { Container, Profile, Welcome, UserName } from './styles';
 
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useTheme();
+
+  const [darkTheme, setDarkTheme] = useState(() =>
+    theme.title === 'dark' ? true : false
+  );
+
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice];
   }, []);
+
+  const HandleChangeTheme = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  };
 
   return (
     <Container>
@@ -16,7 +30,12 @@ const MainHeader: React.FC = () => {
         <Welcome>Olá, {emoji}</Welcome>
         <UserName>Juan Pablo</UserName>
       </Profile>
-      <Toggle />
+      <Toggle
+        labelLeft="Light"
+        labelRight="Dark"
+        checked={darkTheme}
+        onChange={HandleChangeTheme}
+      />
     </Container>
   );
 };
